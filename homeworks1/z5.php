8830e7a6896a159b206a762af58143c47cc190cf
elpvr <elp_vr1@yahoo.com>
2015-06-21 21:11:16 +0300

    Create z5.php

---
+++ homeworks1/z5.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  </head>
+  <body>
+<?php
+  echo 'I asked a girl out and she said – “I '."don't".' know”. Does she mean yes or no?';
+?>
+  </body>
+</html>
