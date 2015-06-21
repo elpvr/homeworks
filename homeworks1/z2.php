88bef5982421244095ae5c52804bcbd4b3e29041
elpvr <elp_vr1@yahoo.com>
2015-06-21 21:09:58 +0300

    Create z2.php

---
+++ homeworks1/z2.php
@@ -0,0 +1,10 @@
+<?php
+  $integer=5;
+  $double=3.14;
+  $string='Name';
+  $boolean=true;
+  echo gettype($integer).'<br/>';
+  echo gettype($double).'<br/>';
+  echo gettype($string).'<br/>';
+  echo gettype($boolean);
+?>
