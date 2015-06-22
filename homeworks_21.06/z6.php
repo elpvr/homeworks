83f17650332132571ffe9a941f4f32ee483750a6
elpvr <elp_vr1@yahoo.com>
2015-06-22 14:21:03 +0300

    Create z6.php

---
+++ homeworks_21.06/z6.php
@@ -0,0 +1,15 @@
+<?php
+  $a=8;
+  switch ($a) {
+    case 1: echo "one"; break;
+    case 2: echo "two"; break;
+    case 3: echo "three"; break;
+    case 4: echo "four"; break;
+    case 5: echo "five"; break;
+    case 6: echo "six"; break;
+    case 7: echo "seven"; break;
+    case 8: echo "eight"; break;
+    case 9: echo "nine"; break;
+    default: echo "Not in range!";
+  }
+?>
