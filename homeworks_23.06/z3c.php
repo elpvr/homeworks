8a321f0bf12845023f80164243502bf1d98de8b8
elpvr <elp_vr1@yahoo.com>
2015-06-27 14:12:38 +0300

    Update z3c.php

--- homeworks_23.06/z3c.php
+++ homeworks_23.06/z3c.php
@@ -20,12 +20,12 @@ foreach ($_SESSION['select'] as $key => $product) {
 echo "      <tr style='border-bottom: 1px solid #FFF'><td colspan='2' align='right'><b>Всичко</b><br/><br/>";
 echo "Наличност по Вашата карта<br/>Остатък след покупката</td>";
 echo "<td align='right'><b>".number_format($total,2)." лв.</b><br/><br/>".number_format($_SESSION['stock'],2);
-echo " лв.<br/>".number_format($_SESSION['stock']-$total,2)." лв.</td></tr>";
+echo " лв.<br/>".number_format($_SESSION['stock']-$total,2)." лв.</td></tr>\n";
 ?>
-      </table>
-      <br/>
-      <a href="z3d.php?confirm=no"><button>Отхвърли</button></a>
-      <a href="z3d.php?confirm=yes"><button style="margin-left: 180px">Потвърди</button></a>
+    </table>
+    <br/>
+    <a href="z3d.php?confirm=no"><button>Отхвърли</button></a>
+    <a href="z3d.php?confirm=yes"><button style="margin-left: 180px">Потвърди</button></a>
 <?php
 include('footer.html');
 ?>
