2f1f31e4ee263f5aa49aed446aeb67cd87bc9619
elpvr <elp_vr1@yahoo.com>
2015-06-29 22:56:02 +0300

    Update z1.php

--- homeworks_28.06/z1.php
+++ homeworks_28.06/z1.php
@@ -1,12 +1,16 @@
 <?php
-  $arr=array('a'=>259,'b'=>56,'c'=>785,'d'=>12,'e'=>-47,'f'=>53);
-  $max=$maxkey='';
+  $arr=array('a'=>259,'b'=>56,'c'=>785,'d'=>60,'e'=>-47,'f'=>53);
+  $max=$maxkey=$submax=$submaxkey='';
   foreach ($arr as $k => $el) {
     if ($max=='' || $el>=$max) {
       $submax=$max;
       $submaxkey=$maxkey;
       $max=$el;
       $maxkey=$k;
+    }
+    elseif ($submax=='' || $el>=$submax) {
+      $submax=$el;
+      $submaxkey=$k;
     }
   }
   echo "$maxkey => $max, $submaxkey => $submax";
