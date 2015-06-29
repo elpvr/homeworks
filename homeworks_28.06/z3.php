59330887469423fe0711527ab327e0f251aa521f
elpvr <elp_vr1@yahoo.com>
2015-06-29 22:34:47 +0300

    Update z3.php

--- homeworks_28.06/z3.php
+++ homeworks_28.06/z3.php
@@ -7,14 +7,19 @@
 <?php
   $text='Поражението на византийците и покоряването от страна на прабългарите на завареното население завършва със сключването на мирен договор през лятото на 681г., чрез който Аспарухова България е призната от Византия.';
   $words=explode(' ',$text);
+  $twocolor=$threecolor='';
   foreach ($words as $key => $word) {
-    switch ($key % 3) {
+    if ($key % 2) { $c="red"; }
+    else { $c="green"; }
+    $twocolor .= "<span style='color: $c'>$word</span> ";
+    switch ($key % 3) { 
       case 0: $c="white"; break;
       case 1: $c="green"; break;
       case 2: $c="red";
     }
-    echo "<span style='color: $c'>$word</span> ";
+    $threecolor .= "<span style='color: $c'>$word</span> ";
   }
+  echo "$twocolor<hr>$threecolor";
 ?>
   </body>
 </html>
