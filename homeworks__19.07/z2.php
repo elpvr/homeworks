5441eccaa026787d68d8c3cc49299f9b37bf3a03
elpvr <elp_vr1@yahoo.com>
2015-07-23 16:35:04 +0300

    Update z2.php

--- homeworks__19.07/z2.php
+++ homeworks__19.07/z2.php
@@ -19,17 +19,17 @@
   if (mysqli_num_rows($res)>0) {
     $i=1;
     while ($dlop=mysqli_fetch_assoc($res)) {
-      echo "      <tr><td>$i</td><td>{$dlop['first_name']}</td><td>{$dlop['last_name']}</td>";
-      echo "<td>{$dlop['race']}</td><td>{$dlop['gender']}</td><td>{$dlop['date_of_birth']}</td>";
-      echo "<td>{$dlop['weight']}</td><td>{$dlop['status']}</td>";
+      echo "      <tr><td>".$i++."</td>";
+      foreach ($dlop as $key => $val) {
+        if ($key != 'id' && $key != 'date_deleted') { echo "<td>$val</td>"; }
+      }
       echo "<td><a href='z2e.php?id={$dlop['id']}' class='btn btn-info'>Edit</a></td>";
       echo "<td><a href='z2d.php?id={$dlop['id']}' class='btn btn-danger'>Delete</a></td>";
       echo "</tr>\n";
-      $i++;
     }
   }
 ?>
     </table>
-    <p class='text-center'><a href='z2a.php' class='btn btn-success'>Add Dlop</a></p>
+    <p class='text-center'><a href='z2a.php?id=0' class='btn btn-success'>Add Dlop</a></p>
   </body>
 </html>
