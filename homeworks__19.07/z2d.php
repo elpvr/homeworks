e9a95316c785e22ddc81cc1d4381c1492c008ae3
elpvr <elp_vr1@yahoo.com>
2015-07-23 16:37:39 +0300

    Update z2d.php

--- homeworks__19.07/z2d.php
+++ homeworks__19.07/z2d.php
@@ -5,19 +5,13 @@
     mysqli_query($link,"UPDATE dlops SET date_deleted=now() WHERE id=$id");
     header('Location: z2.php');
   }
+  $res=mysqli_query($link,"SELECT * FROM dlops WHERE id='{$_GET['id']}'");
+  if (mysqli_num_rows($res)>0) {
+    $dlop=mysqli_fetch_assoc($res);
+    $class="panel-danger";
+    $tit="Delete Dlop";
+    $sbm='Delete';
+    include 'data_form.php';
+  }
+  else { header('Location: z2.php'); }
 ?>
-    <br/>
-    <div class='col-xs-4 col-xs-offset-4'>
-      <div class='panel panel-danger'>
-        <div class='panel-heading'>
-          <h4>Confirm deletion?</h4>
-        </div><br/>
-        <form method="POST" action="z2d.php" class='text-center'>
-          <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>"/>
-          <button type='submit' class='btn btn-success' name="yes">Yes</button> &nbsp; 
-          <a href='z2.php' class='btn btn-danger'>No</a>
-        </form><br/>
-      </div>
-    </div>
-  </body>
-</html>
